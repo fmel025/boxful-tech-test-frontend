84f6e7caafc11a0cb87d19351effc9bd11e4df3e
fmel025 <00018720@uca.edu.sv>
2024-02-03 16:03:26 -0600

    ✨ feat(services): Add state fetch service

---
+++ src/services/stateService.ts
@@ -0,0 +1,12 @@
+import { BASE_URL, STATES_ENDPOINT } from "@/constants/api";
+import { State } from "@/interfaces/state.interface";
+
+export const fetchStates = async (): Promise<State[] | undefined> => {
+  try {
+    const response = await fetch(`${BASE_URL}${STATES_ENDPOINT}`);
+    const data: State[] = await response.json();
+    return data;
+  } catch (error) {
+    console.log("Network error: ", error);
+  }
+};
