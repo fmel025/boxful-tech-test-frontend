9d1cee919c52bb42e484f67395fab6b7e6c1a053
fmel025 <00018720@uca.edu.sv>
2024-02-03 16:28:50 -0600

    🐛 fix: Update enviroment variable name

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,3 +1,3 @@
-export const BASE_URL = process.env.BASE_URL;
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 export const STATES_ENDPOINT = "/states";
 export const ORDERS_ENDPOINT = "/orders";
