400ef8b9d7aabd563afd3a155f7485586a6c0fd6
fmel025 <00018720@uca.edu.sv>
2024-02-03 22:35:11 -0600

    🏷️ feat: Add details interface for DetailsForm

---
+++ src/interfaces/details.interface.ts
@@ -0,0 +1,13 @@
+export interface Details {
+  collectingAddress: string;
+  date: Date;
+  names: string;
+  lastnames: string;
+  email: string;
+  phone: string;
+  recipientAddress: string;
+  state: string;
+  city: string;
+  reference: string;
+  indications: string;
+}
