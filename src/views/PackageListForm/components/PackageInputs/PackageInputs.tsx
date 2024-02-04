f9dbea14ae72415c6fd90710ecd5b04b9697ee8e
fmel025 <00018720@uca.edu.sv>
2024-02-04 16:29:35 -0600

    ♻️ feat(PackageInputs): Add input rules

--- src/views/PackageListForm/components/PackageInputs/PackageInputs.tsx
+++ src/views/PackageListForm/components/PackageInputs/PackageInputs.tsx
@@ -3,40 +3,66 @@ import { Col, Form, Input, Row, InputNumber } from "antd";
 import { IoMdCube } from "react-icons/io";
 import styles from "./PackageInputs.module.css";
 
-function PackageInputs() {
+function PackageInputs({ areDisabled = false}) {
   return (
     <>
       <Row gutter={12}>
         <Col className={styles.flex} span={1}>
-            <IoMdCube className={styles.icon} />
+          <IoMdCube className={styles.icon} />
         </Col>
         <Col span={8}>
           <Row>
             <Col span={8}>
-              <Form.Item label="Largo" name="length">
-                <InputNumber size="large" suffix="cm" />
+              <Form.Item
+                label="Largo"
+                name="length"
+                rules={[{ required: true, message: "" }]}
+              >
+                <InputNumber min={1} size="large" suffix="cm" disabled={areDisabled} />
               </Form.Item>
             </Col>
             <Col span={8}>
-              <Form.Item label="Ancho" name="width">
-                <InputNumber size="large" suffix="cm" />
+              <Form.Item
+                label="Ancho"
+                name="width"
+                rules={[{ required: true, message: "" }]}
+              >
+                <InputNumber min={1} size="large" suffix="cm" disabled={areDisabled} />
               </Form.Item>
             </Col>
             <Col span={8}>
-              <Form.Item label="Alto" name="height">
-                <InputNumber size="large" suffix="cm" />
+              <Form.Item
+                label="Alto"
+                name="height"
+                rules={[{ required: true, message: "" }]}
+              >
+                <InputNumber min={1} size="large" suffix="cm" disabled={areDisabled}/>
               </Form.Item>
             </Col>
           </Row>
         </Col>
         <Col span={4}>
-          <Form.Item label="Peso en libras" name="poundsWeight">
-            <InputNumber className={styles.full_width} size="large" suffix="lb" />
+          <Form.Item
+            label="Peso en libras"
+            name="poundsWeight"
+            rules={[{ required: true, message: "" }]}
+          >
+            <InputNumber
+              min={1}
+              className={styles.full_width}
+              size="large"
+              suffix="lb"
+              disabled={areDisabled}
+            />
           </Form.Item>
         </Col>
         <Col span={11}>
-          <Form.Item label="Contenido" name="content">
-            <Input size="large" />
+          <Form.Item
+            label="Contenido"
+            name="content"
+            rules={[{ required: true, message: "" }]}
+          >
+            <Input min={1} size="large" disabled={areDisabled} />
           </Form.Item>
         </Col>
       </Row>
