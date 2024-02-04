63942f3d49e87f6c6211543fc12afdc455a3be26
fmel025 <00018720@uca.edu.sv>
2024-02-04 15:35:03 -0600

    ✨ feat: Add PackageListForm view

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,13 +7,14 @@ import DetailsForm from "@/views/DetailsForm/DetailsForm";
 import PackageListForm from "@/views/PackageListForm/PackageListForm";
 import { Details } from "@/interfaces/details.interface";
 import { initialDetails } from "@/constants/default-values";
+import { PackageItem } from "@/interfaces/package-item.interface";
 
 const { Content } = Layout;
 
 export default function Home() {
-  const [step, setStep] = useState<number>(1);
+  const [step, setStep] = useState<number>(2);
   const [details, setDetails] = useState<Details>(initialDetails);
-
+  const [packages, setPackages] = useState<PackageItem[]>([]);
 
   // For debugging purposes
   // useEffect(() => {
@@ -22,11 +23,16 @@ export default function Home() {
 
   const onNextStepClick = (values: object) => {
     console.log(values);
-    setDetails(details => ({...details, ...values}));
+    setDetails((details) => ({ ...details, ...values }));
     setStep((step) => step + 1);
   };
 
-  const onPrevStepClick = () => {
+  const onPrevStepClick = (packages: PackageItem[]) => {
+    setPackages([...packages]);
+    setStep((step) => step - 1);
+  };
+
+  const handleSubmit = (packages: PackageItem[]) => {
     setStep((step) => step - 1);
   };
 
@@ -43,7 +49,13 @@ export default function Home() {
           {step === 1 && (
             <DetailsForm initialData={details} onSubmit={onNextStepClick} />
           )}
-          {step === 2 && <PackageListForm />}
+          {step === 2 && (
+            <PackageListForm
+              initialPackages={packages}
+              onSubmit={handleSubmit}
+              onPrevStepClick={onPrevStepClick}
+            />
+          )}
         </div>
       </Content>
     </Layout>
--- src/views/PackageListForm/PackageListForm.tsx
+++ src/views/PackageListForm/PackageListForm.tsx
@@ -1,7 +1,51 @@
-import React from 'react'
+import { Button, Flex, Form } from "antd";
+import "./PackageListForm.css";
+import { ArrowRightOutlined, ArrowLeftOutlined } from "@ant-design/icons";
+import { PackageItem } from "@/interfaces/package-item.interface";
+import { useState } from "react";
+import PackageInputs from "./components/PackageInputs/PackageInputs";
 
-export default function PackageListForm() {
+export default function PackageListForm({
+  initialPackages,
+  onSubmit,
+  onPrevStepClick
+}: {
+  initialPackages: PackageItem[],
+  onSubmit: Function,
+  onPrevStepClick: Function
+}) {
+  const [form] = Form.useForm();
+
+
+  const [packages, setPackages] = useState<PackageItem[]>(initialPackages);
+
+  const handleOnFinish = (data: object) => {
+    console.log(packages);
+    console.log(data);
+  };
+
+  const handleGoPreviousPage = () => {
+    onPrevStepClick(packages);
+  }
+
   return (
-    <div>PackageListForm</div>
-  )
+    <div className="container">
+      <p className="text-container">Agrega tus bultos</p>
+      <Form layout="vertical" className="package-form" form={form} onFinish={handleOnFinish}>
+        <PackageInputs />
+      </Form>
+      
+      {/* Navigation buttons */}
+      <Flex className="btn-group" justify="space-between">
+        <Button size="large" className="btn-back" onClick={handleGoPreviousPage}>
+          <ArrowLeftOutlined />
+          <span>Anterior </span>
+        </Button>
+        <Button size="large" className="btn-send" type="primary">
+          <span>Enviar </span>
+          <ArrowRightOutlined />
+        </Button>
+      </Flex>
+    </div>
+  );
 }
