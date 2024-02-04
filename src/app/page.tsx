14e2f300de930e2cbed5820ccf3dd506234ca65e
fmel025 <00018720@uca.edu.sv>
2024-02-03 20:15:33 -0600

    💄 feat: Add form title and description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,13 +2,28 @@
 
 import { Layout } from "antd";
 import { MainHeader } from "@/components";
+import { useForm } from "antd/es/form/Form";
+import { useState } from "react";
 
-const { Header } = Layout;
+const { Content } = Layout;
 
 export default function Home() {
+  const [form] = useForm();
+  const [step, setStep] = useState<number>(1);
+
   return (
     <Layout>
       <MainHeader />
+      <Content style={{ margin: "40px auto", width: "75%" }}>
+        <div>
+          <h2 className="title">Crea una orden</h2>
+          <p className="title-text">
+            Dale una ventaja competitiva a tu negocio con entregas el mismo día
+            (Área Metropolitana) y el día siguiente a nivel nacional.
+          </p>
+        </div>
+        
+      </Content>
     </Layout>
   );
 }
